442cc22b4c5a47bddb87c6519c29b346b25afd05
paovfang <paovfang@gmail.com>
2014-03-28 21:46:47 -0700

    updated jparser
    updated jparser as well as deleted gson, converted to using jackson for
    json parsing

---
+++ frontend/VotingSystem/src/Ballot.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ballot {
+	private String state;
+	private String vid;
+	private List<String> proposition;
+	private List<String> presidential_candidates;
+	
+}
--- frontend/VotingSystem/src/JParser.java
+++ frontend/VotingSystem/src/JParser.java
@@ -1,106 +1,50 @@
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Iterator;
-import java.util.Map.Entry;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JParser {
-	final private String path = "."; //set directory path
-    private JsonParser parser = new JsonParser();
-    private int count = -1;
-
+	
+	public static class Proposition {
+        public String proposition_number;
+        public String question;
+        public int id;
+    }
+	
+	String testObjects;
+	
     public JParser() {
         //JParser read = new JParser();
-        sendJSONFile();
+        //readJSONFile();
+    	
+    	try {
+    		BufferedReader fileReader = new BufferedReader(new FileReader("./test.json"));
+    		ObjectMapper mapper = new ObjectMapper();
+			JsonNode node = mapper.readTree(fileReader);
+			String state = node.path("state").textValue();
+			String vid = node.path("vid_hash").textValue();
+			//String prop = node.path("proposition").textValue();
+			System.out.printf("%s\n%s\n", state, vid);
+			
+			JsonNode propNode = node.get("proposition");
+			TypeReference<List<Proposition>> typeRef = new TypeReference<List<Proposition>>(){};
+	        List<Proposition> list = mapper.readValue(propNode.traverse(), typeRef);
+	        for (Proposition f : list) {
+	            System.out.printf("%s\n%s\n%d\n", f.proposition_number, f.question, f.id);
+	        }
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     }
-
-    //send json files to read one by one
-    private void sendJSONFile() {
-
-        //File[] jsonfiles = getJSONFiles();
-
-        //if (jsonfiles.length > 0) {
-        //    for (int i = 0; i <= count; i++) {
-
-                try {
-                    //prints json file names
-                    //System.out.println("File: \t" + jsonfiles[i]);
-                    JsonElement jsonElement = parser.parse(new FileReader("./test.json"));
-                    JsonObject jsonObject = jsonElement.getAsJsonObject();
-                    readJSONFile(jsonObject);
-                }
-                catch (FileNotFoundException e) {
-                    System.out.println(e.getMessage());
-                    //e.getStackTrace();
-                }
-                catch (IOException e) {
-                    System.out.println(e.getMessage());
-                    //e.getStackTrace();
-                }
-                catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    //e.getStackTrace();
-                }
-
-          //  }
-        //}
-    }
-
-    //read a complete json file
-    private void readJSONFile(JsonObject jsonObject) {
-
-        for (Entry<String, JsonElement> entry : jsonObject.entrySet()) {
-
-            String key = entry.getKey();
-            JsonElement value = entry.getValue();
-
-            if (value.isJsonObject()) {
-                readJSONFile(value.getAsJsonObject());
-            }
-            else if (value.isJsonArray()) {
-                 JsonArray jsonArray = value.getAsJsonArray();
-           
-                 if (jsonArray.size() == 1) {
-                    readJSONFile((JsonObject) jsonArray.get(0));
-                }
-                 else {
-                    //prints json array name
-                    System.out.println(key);
-                    Iterator<JsonElement> msg = jsonArray.iterator();
-                    while (msg.hasNext()) {
-                        ////prints json array values
-                        System.out.println(msg.next());
-                    }
-                }
-            }
-            else {
-                ////prints json object's keys and values
-                System.out.println(key + " - " + value);
-            }
-        }
-    }
-
-    //get only .json files from a directory
-    private File[] getJSONFiles() {
-
-        File folder = new File(path);
-        File[] files = folder.listFiles();
-        File[] jsonfiles = new File[files.length];
-
-        for (int i = 0; i < files.length; i++) {
-
-            if (files[i].isFile()) {
-                if (files[i].getName().endsWith(".json") || files[i].getName().endsWith(".JSON")) {
-                    jsonfiles[++count] = files[i];
-                }
-            }
-        }
-
-        return files;
-    }
+    
 }
