c929334e987199bd962565b8d8f6abc5076bf162
mrgarrett <michaelgarrett@csus.edu>
2014-03-30 16:07:00 -0700

    working on java/python interface
    jthon stuff

--- connection/client.py
+++ connection/client.py
@@ -6,12 +6,19 @@ import socket
 TCP_IP = '127.0.0.1' # IP of the server
 TCP_PORT = 9999
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+# MESSAGE = "Hello, World!"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
+def connect():
+	print("Connecting ...")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+def set(MESSAGE):
+	print("Sending Message ...")
+	s.send(MESSAGE)
+def get():
+	print("Receivng Data ...")
+	data = s.recv(BUFFER_SIZE)
+	return data
 s.close()
 
 print "received data:", data
