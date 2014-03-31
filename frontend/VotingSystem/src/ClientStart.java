0084e22fc7c4b6733ce76cdff83697a41456789a
paovfang <paovfang@gmail.com>
2014-03-31 01:29:20 -0700

    returned clientstart to normal
    This is the main clientstart for the frontend, the clientstart.java in
    jython_test is for testing jython

--- frontend/VotingSystem/src/ClientStart.java
+++ frontend/VotingSystem/src/ClientStart.java
@@ -1,19 +1,9 @@
-import org.python.core.PyInteger;
-import org.python.core.PyObject;
-import org.python.util.PythonInterpreter;
 
 public class ClientStart {
 
 	public static void main(String[] args) {
-		//Login lg = new Login();
+		Login lg = new Login();
 		//VotingBallot vb = new VotingBallot();
-		PythonInterpreter interpreter = new PythonInterpreter();
-		interpreter.exec("from test import tmp");
-		PyObject someFunc = interpreter.get("tmp");
-		//System.out.println(someFunc);
-		PyObject result = someFunc.__call__(new PyInteger(5), new PyInteger(7));
-		int realResult = (int) result.__tojava__(Integer.class);
-		System.out.println(realResult);
 	}
 
 }
